9d292c02390654d01f15ca7c3a3b09161d316d9c
kimkyusoo <110078419+kimkyusoo@users.noreply.github.com>
2022-08-03 11:13:29 +0900

    Update app.py

--- category/app.py
+++ category/app.py
@@ -1,20 +1,87 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://gogotest:spa0727rtan@cluster0.xukpzid.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta
-
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
 
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('mongodb://3.34.190.221', 27017, username="test", password="test")
+db = client.dbsparta_plus_signup
+
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-@app.route('/all')
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+        return render_template('category.html')
+
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,
+        "password": password_hash,
+        "profile_name": username_receive,
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+@app.route('/category')
 def all():
-   return render_template('all.html')
+   return render_template('category.html')
 
 @app.route('/Gangneung')
 def gangneung():
@@ -64,12 +131,10 @@ def food_post():
 
     return jsonify({'msg':'맛집 등록 완료!'})
 
-@app.route("/gangwonfood", methods=["GET"])
+@app.route("/Gangwonfood", methods=["GET"])
 def gangwon_get():
     foods_list = list(db.foods.find({},{'_id':False}))
     return jsonify({'movies':foods_list})
-
-
 
 @app.route("/Gangneungfood", methods=["GET"])
 def Gangneung_get():
@@ -93,4 +158,4 @@ def Yangyang_get():
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
