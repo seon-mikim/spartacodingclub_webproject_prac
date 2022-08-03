5fc24d3cac515c38ac347482f4c3e0c2f9f12437
yongs7 <110363987+yongs7@users.noreply.github.com>
2022-08-03 17:07:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,26 +83,26 @@ def check_dup():
 def all():
    return render_template('category.html')
 
-@app.route('/Gangneung')
+@app.route('/Gangneung.html')
 def gangneung():
    return render_template('Gangneung.html')
 
-@app.route('/Sokcho')
+@app.route('/Sokcho.html')
 def Sokcho():
    return render_template('Sokcho.html')
 
-@app.route('/Yangyang')
+@app.route('/Yangyang.html')
 def Yangyang():
    return render_template('Yangyang.html')
 
-@app.route('/Donghae')
+@app.route('/Donghae.html')
 def Donghae():
    return render_template('Donghae.html')
 
 
 
-@app.route("/food", methods=["POST"])
-def food_post():
+@app.route("/place", methods=["POST"])
+def place_post():
     url_receive = request.form['url_give']
     star_receive = request.form['star_give']
     comment_receive = request.form['comment_give']
@@ -134,27 +134,27 @@ def food_post():
 @app.route("/Gangwonfood", methods=["GET"])
 def gangwon_get():
     foods_list = list(db.foods.find({},{'_id':False}))
-    return jsonify({'movies':foods_list})
+    return jsonify({'place':foods_list})
 
 @app.route("/Gangneungfood", methods=["GET"])
 def Gangneung_get():
     foods_list = list(db.foods.find({'area':'강릉'}, {'_id': False}))
-    return jsonify({'movies': foods_list})
+    return jsonify({'place': foods_list})
 
 @app.route("/Donghaefood", methods=["GET"])
 def Donghae_get():
     foods_list = list(db.foods.find({'area':'동해'}, {'_id': False}))
-    return jsonify({'movies': foods_list})
+    return jsonify({'place': foods_list})
 
 @app.route("/Sokchofood", methods=["GET"])
 def Sokcho_get():
     foods_list = list(db.foods.find({'area':'속초'}, {'_id': False}))
-    return jsonify({'movies': foods_list})
+    return jsonify({'place': foods_list})
 
 @app.route("/Yangyangfood", methods=["GET"])
 def Yangyang_get():
     foods_list = list(db.foods.find({'area':'양양'}, {'_id': False}))
-    return jsonify({'movies': foods_list})
+    return jsonify({'place': foods_list})
 
 
 if __name__ == '__main__':
